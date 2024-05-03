9604c737c2d11b1a4f27e973c63c37901bb31833
Ashish <rbzashu1@gmail.com>
2024-05-03 13:57:19 +0530

    created home page and pricing page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./page.css";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="home_container">
+      <div>
+        <h1>
+          You Travel The World. <br />
+          WorldWise Keep Track Of Your Adventures
+        </h1>
+        <p className="para">
+          Embark on unforgettable adventures, effortlessly keep tabs on your
+          travels, and cherish every moment along the way with our intuitive
+          travel app designed to accompany you on your globetrotting escapades!
+        </p>
+      </div>
+      <div className="button" style={{ display: "flex", alignSelf: "center" }}>
+        <button>Start Tracking Now</button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
